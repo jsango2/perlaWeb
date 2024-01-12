af537aad3c0a38bf8eb05ba46340d63f3c704219
jsango2 <jure.sango@gmail.com>
2024-01-12 14:10:52 +0100

    /recepti page

--- pages/pop.js
+++ pages/pop.js
@@ -29,6 +29,10 @@ export default function IndexPage({ allRecepti }) {
       ></Script>
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta
+        http-equiv="Content-Security-Policy"
+        content="upgrade-insecure-requests"
+      />
+      <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
       />
